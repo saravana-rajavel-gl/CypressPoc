722ac2a874bef71c44cd3b211fbc77e793ae9ae7
vkrishna-GL <pyna.krishna@globallogic.com>
2022-08-17 13:14:59 +0530

    Modified code for multi iframes

--- cypress/integration/CypressPOC/iframe.spec.js
+++ cypress/integration/CypressPOC/iframe.spec.js
@@ -1,33 +1,52 @@
-import 'cypress-iframe';
-require('cypress-xpath');
+import 'cypress-iframe'
 
-var URLa="";
-describe('example to-do app', () => {
-   
-  it('Should vist iframe!', () => {
+describe('working in iFrames', function () {
+    it('method', function () {
+        cy.visit('index.html')
+        cy.wait(2000)
+        getIframeBody().find('#A2').invoke('removeAttr', 'target').click()
+        cy.wait(2000)
+        getIframeBody().find('#search2').type('cypress')
+        getIframeBody().find('#listofsearchresults a').click()
+    })
 
-    cy.visit('index.html')
-    cy.frameLoaded('#iframe_a');
-    cy.iframe('#iframe_a').find('#A2').should('be.visible').click();
-  })
-  it('iframe 2 contents check', () => {    
-    cy.iframe('#iframe_b').find('#sb_form_q').should('be.visible').type('Hello, World')  
-    cy.iframe('#iframe_b').find('#search_icon').should('be.visible').click();
-  })
-  it('iframe Multi tabs issue', () => {    
-    cy.iframe('#iframe_a').find('#A1').should('be.visible')
-    cy.iframe('#iframe_a').find('#A1').should('be.visible').each(($el) => {
-      const herf = $el.attr('href');
-      URLa=herf;
-      cy.log(URLa);
-      cy.log(herf);
-      cy.iframe('#iframe_b').invoke('attr', 'src', herf)      
-      cy.frameLoaded('#iframe_b');
-      })
-  })
-  it('iframe Multi tabs issue1', () => {
-    cy.pause();
-    cy.visit(URLa);
-    cy.log(URLa);
-  })
-});
+    it('method', function () {
+        cy.visit('index.html')
+        cy.wait(2000)
+        //getIframeBody().find('#A1').invoke('removeAttr', 'target').click()
+        getIframeBody().find('#A1').click();
+        cy.wait(2000)
+        getIframeBody2().find('#sb_form_q').type('cypress')
+        getIframeBody2().find('#sa_ul li').eq(1).click()
+    })
+
+    const getIframeDocument = () => {
+        return cy
+            //.get('iframe[data-cy="the-frame"]')
+            .get('#iframe_a')
+            .its('0.contentDocument').should('exist')
+    }
+
+    const getIframeBody = () => {
+        // get the document
+        return getIframeDocument()
+            // automatically retries until body is loaded
+            .its('body').should('not.be.undefined')
+            .then(cy.wrap)
+    }
+
+    const getIframeDocument2 = () => {
+        return cy
+            //.get('iframe[data-cy="the-frame"]')
+            .get('#iframe_b')
+            .its('0.contentDocument').should('exist')
+    }
+
+    const getIframeBody2 = () => {
+        // get the document
+        return getIframeDocument2()
+            // automatically retries until body is loaded
+            .its('body').should('not.be.undefined')
+            .then(cy.wrap)
+    }
+})
